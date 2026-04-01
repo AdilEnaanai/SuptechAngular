210918b4d136f0d892147465c90989962094bfa9
ENAANAI <aenaanai@uae.ac.ma>
2026-04-01 16:16:58 +0100

    Routers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Accueil } from './accueil/accueil';
+import { Produits } from './produits/produits';
+import { Services } from './services/services';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:"accueil",component:Accueil},
+    {path:"produits",component:Produits},
+    {path:"services",component:Services},
+    {path:"contact",component:Contact},
+    {path:"",redirectTo:"accueil",pathMatch:"full"}
+];
